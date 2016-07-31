a2b3d6441170533fc854ba3f2eca9ca88b104151
xifeng.cao <xifeng.cao@yinyuetai.com>
2016-08-01 03:02:02 +0800

    修改提取scss不规范

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,6 @@ var fs = require('fs');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var extractSASS = new ExtractTextPlugin('[name].css', {
-    allChunks: true
-});
 
 
 // webpack扩展功能
@@ -49,12 +46,12 @@ module.exports = {
             // 编译css并自动添加css前缀
             {
                 test: /\.css$/,
-                loader: extractSASS.extract('style-loader', 'css-loader?sourceMap')
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap')
             },
             //.scss 文件想要编译，scss就需要这些东西！来编译处理
             {
                 test: /\.scss$/,
-                loader: extractSASS.extract('style-loader','css-loader?sourceMap!sass-loader')
+                loader: ExtractTextPlugin.extract('style-loader','css-loader?sourceMap!sass-loader')
             }, {
                 test: /\.(png|jpg|gif)$/,
                 loader: 'url-loader?limit=8192&name=images/[name].[ext]'
@@ -80,12 +77,14 @@ module.exports = {
     },
     vue: {
         loaders: {
-            css: extractSASS.extract("vue-style-loader", "css-loader?sourceMap"),
-            scss: extractSASS.extract("vue-style-loader", "css-loader?sourceMap", "sass-loader"),
+            css: ExtractTextPlugin.extract("vue-style-loader", "css-loader?sourceMap"),
+            scss: ExtractTextPlugin.extract("vue-style-loader", "css-loader?sourceMap", "sass-loader"),
         }
     },
     plugins: [
-        extractSASS,
+        new ExtractTextPlugin('[name].css', {
+            allChunks: true
+        }),
         new webpack.optimize.CommonsChunkPlugin('common', 'common.js')
     ]
 };
